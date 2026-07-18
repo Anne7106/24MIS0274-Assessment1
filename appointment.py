2889c35cead30fe561df597431e267a0c7f3b9f6
Anne 7106 <annecatherine.c2024@vitstudent.ac.in>
2026-07-18 20:44:34 +0530

    Login added

--- appointment.py
+++ appointment.py
@@ -1,2 +1,10 @@
-print("Hospital Appointment Booking System")
-print("Case study 2")
+username="AAAA"
+password="xxxx"
+u=input("Enter username:")
+p=input("Enter password:")
+if u==username and p==password:
+    print("Logged in successfully")
+    print("Hospital Appointment Booking System")
+    print("Case study 2")
+else:
+    print("Incorrect username or password")
