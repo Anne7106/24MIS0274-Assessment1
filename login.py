0811453d22816b03a01f013645e10aaf1714e8e4
Anne 7106 <annecatherine.c2024@vitstudent.ac.in>
2026-07-18 20:00:54 +0530

    Login module done

---
+++ login.py
@@ -0,0 +1,8 @@
+username="AAAA"
+password="xxxx"
+u=input("Enter username:")
+p=input("Enter password:")
+if u==username and p==password:
+    print("Logged in successfully")
+else:
+    print("Incorrect username or password")
