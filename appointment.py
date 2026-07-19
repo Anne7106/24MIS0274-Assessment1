60e75c1afebd1a3768ab62e5814c42648f2d5eba
Anne 7106 <annecatherine.c2024@vitstudent.ac.in>
2026-07-19 11:55:02 +0530

    Added patient login

--- appointment.py
+++ appointment.py
@@ -5,6 +5,7 @@ p=input("Enter password:")
 if u==username and p==password:
     print("Logged in successfully")
     print("Hospital Appointment Booking System")
-    print("Case study 2")
+    print("Patient Login")
 else:
     print("Incorrect username or password")
+print("Feature change branch")
